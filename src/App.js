53ec9cdd5affa616bd0d5cabdb40daede493e45e
Jaime Villegas <jaimevillegas296@gmail.com>
2023-04-03 11:51:37 -0500

    First part of the tutorial

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import NavBar from "./components/Navbar";
+import CartContainer from "./components/CartContainer";
+
+function App() {
+  return <main>
+    <NavBar />
+    <CartContainer />
+  </main>;
+}
+export default App;
